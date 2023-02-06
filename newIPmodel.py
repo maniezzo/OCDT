b3685e565fdf494763467209a060fdd4a87b48f8
Vittorio Maniezzo <vittorio.maniezzo@unibo.it>
2023-02-06 12:33:07 +0100

    feb 6 mattina

--- newIPmodel.py
+++ newIPmodel.py
@@ -46,27 +46,65 @@ def intersect(jp):
          break
    return fIntersect
 
-# reduces AABB given point of different class. Return false if impossible
+# initializes a new AABB with the point with index in argument
+def initializeAABB(ip):
+   minlo = np.zeros(ndim)  # minimum (tighest) lower extent
+   minhi = np.zeros(ndim)  # minimum (tighest) upper extent
+   maxlo = np.zeros(ndim)  # maximum (widest)  lower extent
+   maxhi = np.zeros(ndim)  # maximum (widest)  upper extent
+   for i in np.arange(ndim):
+      maxlo[i] = np.min(X[:, i]) - 1  # going to infinite
+      maxhi[i] = np.max(X[:, i]) + 1
+      minlo[i] = X[ind[idp, k], i]
+      minhi[i] = X[ind[idp, k], i]
+   return minlo,minhi,maxlo,maxhi
+
+# recompute bbox
+def recomputeAABB():
+   for dim in np.arange(ndim):
+      minhi[dim] = minlo[dim] = X[lstPoints[0], dim]  # recompute tight bbox
+      for lp in lstPoints:
+         if (X[lp, dim] > minhi[dim]): minhi[dim] = X[lp,dim]
+         if (X[lp, dim] < minlo[dim]): minlo[dim] = X[lp,dim]
+
+# the bbox given current min values
+def AABB2bbox():
+   bbox = np.zeros(4 * ndim).reshape(ndim, 4)  # rows: dim, cols: maxlo minlo minh maxhi
+   for i in np.arange(ndim):
+      bbox[i, 0] = maxlo[i]
+      bbox[i, 1] = minlo[i]
+      bbox[i, 2] = minhi[i]
+      bbox[i, 3] = maxhi[i]
+   return bbox
+
+# remove fromlstPoints all those incompoatible with jp along direction dim
+def pruneLstPoints(jp,dim):
+   for lp in lstPoints:  # patch, if I find an incompatible presented too late
+      if (X[lp, dim] == X[jp, dim] and X[lp, dim] == minlo[dim]):
+         maxlo[dim] = X[lp, dim]
+         print(f"Removing {lp} from {lstPoints} (incompatible with {jp})")
+         lstPoints.remove(lp)  # confliting internal point
+      if (X[lp, dim] == X[jp, dim] and X[lp, dim] == minhi[dim]):
+         maxhi[dim] = X[lp, dim]
+         print(f"Removing {lp} from {lstPoints} (incompatible with {jp})")
+         lstPoints.remove(lp)  # confliting internal point
+
+# reduces AABB given point jp of different class. Return false if impossible
 def reduceAABB(jp,dir):
    for dim in np.arange(ndim):
       if X[jp,dim] > minlo[dim] and X[jp, dim] < minhi[dim]:
-         return False
+         fIntersect = intersect(jp)
+         if(fIntersect):
+            pruneLstPoints(jp,dir)
+            recomputeAABB()
+         return False # cannot reduce, point incompatible
       if dim != dir: # do not reduce on the explored dimension
          if (X[jp,dim] < minlo[dim] and X[jp,dim] > maxlo[dim]): maxlo[dim] = X[jp,dim]
          if (X[jp,dim] > minhi[dim] and X[jp,dim] < maxhi[dim]): maxhi[dim] = X[jp,dim]
-      else: # if I find an incompatible presented too late
-         for lp in lstPoints:
-            if (X[lp, dim] == X[jp,dim] and X[lp, dim] == minlo[dim]):
-               maxlo[dim] = X[lp, dim]
-               lstPoints.remove(lp)  # confliting internal point
-            if (X[lp, dim] == X[jp,dim] and X[lp, dim] == minhi[dim]):
-               maxhi[dim] = X[lp, dim]
-               print(f"Removing {lp} from {lstPoints}")
-               lstPoints.remove(lp)  # confliting internal point
-         minhi[dim] = minlo[dim] = X[lstPoints[0], dim]
-         for lp in lstPoints:
-             if(X[lp,dim]>minhi[dim]): minhi[dim]=X[lp,dim]
-             if(X[lp,dim]<minlo[dim]): minlo[dim]=X[lp,dim]
+      else: #dim = dir
+         if(intersect(jp)):
+            pruneLstPoints(jp,dim)
+         recomputeAABB()
    return True
 
 # enlarges AABB given point of same class. Return false if impossible
@@ -80,7 +118,6 @@ def enlargeAABB(jp):
 
 # adds (if it is the case) a bbox the the bbox list
 def addBB(bbox):
-   a = 3 + 2
    for b in np.arange(len(lstAABB)):
       isContained = True
       fContains   = True
@@ -89,10 +126,13 @@ def addBB(bbox):
             isContained = False
          if bbox[d,1] > lstAABB[b][d,1] or bbox[d,2] < lstAABB[b][d,2]:
             fContains = False
-      if isContained: return
+      if isContained:
+         print("bbox contained, no appending")
+         return
       if fContains:
          lstAABB[b] = copy.deepcopy(bbox)
    lstAABB.append(copy.deepcopy(bbox))
+   print(f"New AAB: {bbox}")
 
 if __name__ == "__main__":
    os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -128,25 +168,17 @@ if __name__ == "__main__":
    for idperm in np.arange(len(lstPrmt)): # per ogni permutazine delle dimensioni
       for k in lstPrmt[idperm]:      # dimensione corrente
          for idp in np.arange(n):    # indice punto corrente
-            print(f">>>> point {ind[idp,k]} dir {k}")
-            minlo  = np.zeros(ndim)  # minimum (tighest) lower extent
-            minhi  = np.zeros(ndim)  # minimum (tighest) upper extent
-            maxlo  = np.zeros(ndim)  # maximum (widest)  lower extent
-            maxhi  = np.zeros(ndim)  # maximum (widest)  upper extent
-            for i in np.arange(ndim):
-               maxlo[i] = np.min(X[:,i])-1 # going to infinite
-               maxhi[i] = np.max(X[:,i])+1
-               minlo[i] = X[ind[idp,k],i]
-               minhi[i] = X[ind[idp,k],i]
+            print(f">>>> new bbox on point {ind[idp,k]} dir {k}")
+            minlo, minhi, maxlo, maxhi = initializeAABB(ind[idp,k])
             # ordinamento sulla dimensione k, bbox con tutte le altre
             cls = df.iloc[ind[idp,k],3]
             lstPoints = [ind[idp,k]] # points in current cluster
 
             j = idp + 1
             while j < n:
-               intr = intersect(ind[j,k])  # useless?
                clspt = df.iloc[ind[j,k],3] # class of current point j
                if(clspt!=cls):
+                  intr = intersect(ind[j,k])  # useless?
                   if reduceAABB(ind[j,k],k):
                      print(f"reducing cause of {ind[j,k]} ({X[ind[j,k],0]},{X[ind[j,k],1]})")
                   else:
@@ -159,11 +191,7 @@ if __name__ == "__main__":
                   print(f"adding {ind[j,k]} ({X[ind[j,k],0]},{X[ind[j,k],1]})")
                print(f"bbox cls:{cls}: idpt {ind[j,k]} class {clspt} x:{maxlo[0]}/{minlo[0]}/{minhi[0]}/{maxhi[0]} y:{maxlo[1]}/{minlo[1]}/{minhi[1]}/{maxhi[1]}")
                j+=1
-            bbox = np.zeros(4*ndim).reshape(ndim,4) # rows: dim, cols: maxlo minlo minh maxhi
-            for i in np.arange(ndim):
-               bbox[i,0] = maxlo[i]
-               bbox[i,1] = minlo[i]
-               bbox[i,2] = minhi[i]
-               bbox[i,3] = maxhi[i]
+            bbox = AABB2bbox()
             addBB(bbox)
+   print("... END")
    pass
