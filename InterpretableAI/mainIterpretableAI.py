5888a898294b5580772c700fe040cf808f238e64
Vittorio Maniezzo <vittorio.maniezzo@unibo.it>
2023-05-30 12:55:48 +0200

    boh

--- InterpretableAI/mainIterpretableAI.py
+++ InterpretableAI/mainIterpretableAI.py
@@ -1,7 +1,7 @@
 import interpretableai.installation
 import sys
 from interpretableai.installation import (install, install_julia, install_system_image,
-                           cleanup_installation)
+                                          cleanup_installation)
 from interpretableai.predictor import Predictor
 # Julia executable: C:\Users\vittorio.maniezzo\AppData\Local\InterpretableAI\InterpretableAI\julia\1.8.5\julia-1.8.5\bin\julia.exe
 import interpretableai.iai as iai
@@ -14,14 +14,50 @@ X = df.iloc[:,0:3]
 y = df.iloc[:,4]
 (train_X, train_y), (test_X, test_y) = iai.split_data("classification", X, y,seed=1)
 
+print("CART")
 grid = iai.GridSearch(
     iai.OptimalTreeClassifier(
         random_seed=1,
+        localsearch=False,
+        missingdatamode='separate_class',
+        criterion='gini',
     ),
-    max_depth=range(1, 6),
+    max_depth=range(1, 10),
+)
+grid.fit(train_X, train_y)
+cartlnr = grid.get_learner()
+
+print("Testing without hyperplanes")
+grid = iai.GridSearch(
+   iai.OptimalTreeClassifier(
+      random_seed=1,
+   ),
+   max_depth=range(1, 6),
+)
+grid.fit(train_X, train_y)
+print(grid.get_grid_result_summary())
+lnr = grid.get_learner()
+iai.load_graphviz()
+lnr.write_png("learner.png")
+param = grid.get_best_params()
+score1 = grid.score(test_X, test_y, criterion='auc')
+print(f"No hyp score={score1}")
+
+treeplot = lnr.TreePlot()
+lnr.write_json("tree.json")
+lnr.write_html("tree.html")
+
+print("Testing with hyperplanes")
+grid = iai.GridSearch(
+   iai.OptimalTreeClassifier(
+      random_seed=1,
+      max_depth=2,
+      hyperplane_config={'sparsity': 'all'}
+   ),
 )
 grid.fit(train_X, train_y)
 grid.get_learner()
-score = grid.score(test_X, test_y, criterion='auc')
-fig = grid.ROCCurve(test_X, test_y, positive_label=1)
+score2 = grid.score(test_X, test_y, criterion='auc')
+print(f"Hyp score={score2}")
+
 print("Fine")
