bb141605b81e76ca0591815c8ef0c34c8d119331
Vittorio Maniezzo <vittorio.maniezzo@unibo.it>
2023-07-24 11:55:59 +0200

    dfs

--- plotTree/c++/plotTree/plotTree.cpp
+++ plotTree/c++/plotTree/plotTree.cpp
@@ -1,5 +1,6 @@
 #include "plotTree.h"
 #include <windows.h>    // GetModuleFileName
+#include <stack>
 
 void Tree::goTree()
 {  vector<int> dummy;
@@ -10,6 +11,42 @@ void Tree::goTree()
    readData(dataSetFile);
    regionBitmasks();
    contingency3D();
+}
+
+// prints all not yet visited vertices reachable from s
+void Tree::DFS(int s)
+{
+   // Initially mark all vertices as not visited
+   vector<bool> visited(V, false);
+
+   // Create a stack for DFS
+   stack<int> stack;
+
+   // Push the current source node.
+   stack.push(s);
+
+   while (!stack.empty())
+   {
+      // Pop a vertex from stack and print it
+      int s = stack.top();
+      stack.pop();
+
+      // Stack may contain same vertex twice. So
+      // we need to print the popped item only
+      // if it is not visited.
+      if (!visited[s])
+      {
+         cout << s << " ";
+         visited[s] = true;
+      }
+
+      // Get all adjacent vertices of the popped vertex s
+      // If a adjacent has not been visited, then push it
+      // to the stack.
+      for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
+         if (!visited[*i])
+            stack.push(*i);
+   }
 }
 
 // number of cases per cut and per value
